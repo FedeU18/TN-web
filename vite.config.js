85cfee58f1c6f3752e693090e08c5f4b1a9350db
Federico <fedee.unates.2001@gmail.com>
2025-11-16 21:44:44 -0300

    a ver si deploya ahora

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Proxificar las peticiones a la API y al WSDL SOAP hacia el backend durante el desarrollo
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-      "/wsdl": {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+  // Cargar variables de entorno correctas para dev/prod
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/wsdl": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-});
+  });
+};
